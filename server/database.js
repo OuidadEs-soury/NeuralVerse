3338f3354e14a03b03d3ebfda9ba800056970531
Ouidad7 <widadelsouri@gmail.com>
2026-04-04 14:44:30 +0100

    ultra mode

--- server/database.js
+++ server/database.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose=require("mongoose")
 
 async function connectDB(){
 
-await mongoose.connect("mongodb://127.0.0.1:27017/neuralverse")
+await mongoose.connect("mongodb://127.0.0.1:27017/galaxy")
 
-console.log("🌌 database connected")
+console.log("database connected")
 
 }
 
-module.exports = connectDB
+module.exports=connectDB
