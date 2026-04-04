3a57c63842b7d3aa57fa794d8199c7cfd9b53ef9
Ouidad7 <widadelsouri@gmail.com>
2026-04-04 14:47:11 +0100

    Server update

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const cors=require("cors")
 
 const connectDB=require("./database")
 
-const worldRoutes=require("./routes/worlds")
-const aiRoutes=require("./routes/ai")
+const Planet=require("./models/Planet")
+const generateLore=require("./ai")
 
 const app=express()
 
@@ -17,11 +17,36 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
-app.use("/api/worlds",worldRoutes)
-app.use("/api/ai",aiRoutes)
+app.post("/api/planet",async(req,res)=>{
+
+const name=req.body.name
+
+const lore=await generateLore(name)
+
+const planet=new Planet({
+
+name:name,
+type:"generated",
+description:lore
+
+})
+
+await planet.save()
+
+res.json(planet)
+
+})
+
+app.get("/api/planets",async(req,res)=>{
+
+const planets=await Planet.find()
+
+res.json(planets)
+
+})
 
 app.listen(3000,()=>{
 
-console.log("🚀 NeuralVerse Ultra running")
+console.log("Galaxy God Mode running")
 
 })
