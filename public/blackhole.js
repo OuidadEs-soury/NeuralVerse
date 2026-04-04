8b4212ae9c55c2d3de03d9e4ec584c4439f34731
Ouidad7 <widadelsouri@gmail.com>
2026-04-04 15:18:40 +0100

    Blackhole Update

--- public/blackhole.js
+++ public/blackhole.js
@@ -1,13 +1,25 @@
-const geo=new THREE.SphereGeometry(3,64,64)
+const geo = new THREE.SphereGeometry(5,64,64)
 
-const mat=new THREE.MeshBasicMaterial({
+const mat = new THREE.MeshBasicMaterial({
 
 color:0x000000
 
 })
 
-const blackhole=new THREE.Mesh(geo,mat)
+const blackhole = new THREE.Mesh(geo,mat)
 
-blackhole.position.set(30,0,0)
+blackhole.position.set(0,0,-80)
 
 scene.add(blackhole)
+
+function blackHoleGravity(){
+
+const distance = ship.position.distanceTo(blackhole.position)
+
+if(distance < 50){
+
+ship.position.lerp(blackhole.position,0.01)
+
+}
+
+}
