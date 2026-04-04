ee258368c7db0d63fc42ef6124985472d154911b
Ouidad7 <widadelsouri@gmail.com>
2026-04-04 15:19:22 +0100

    Controle

--- public/controls.js
+++ public/controls.js
@@ -1,29 +1,37 @@
-const keys = {}
+const keys={}
 
-const speed = 1
+let speed=1
 
-document.addEventListener("keydown", e => {
+document.addEventListener("keydown",e=>{
+
 keys[e.key.toLowerCase()] = true
+
 })
 
-document.addEventListener("keyup", e => {
+document.addEventListener("keyup",e=>{
+
 keys[e.key.toLowerCase()] = false
+
 })
 
 function updateControls(){
 
+if(keys["shift"]) speed=3
+else speed=1
+
 if(keys["w"]) ship.position.z -= speed
-
 if(keys["s"]) ship.position.z += speed
-
 if(keys["a"]) ship.position.x -= speed
-
 if(keys["d"]) ship.position.x += speed
 
 camera.position.x = ship.position.x
-camera.position.z = ship.position.z + 10
-camera.position.y = ship.position.y + 5
+camera.position.z = ship.position.z+10
+camera.position.y = ship.position.y+5
 
 camera.lookAt(ship.position)
+
+blackHoleGravity()
+
+checkPlanetLanding()
 
 }
